50544c685ad029ea2dc3c15121636243b744fa2a
Anja Leichsenring <anja.leichsenring@typo3.com>
2017-05-20 17:00:09 +0200

    Add b/w compat classes

---
+++ compat/core/ActionService.php
@@ -0,0 +1,20 @@
+<?php
+namespace TYPO3\CMS\Core\Tests\Functional\DataHandling\Framework;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * @deprecated since TYPO3 9, will be removed in TYPO3 10
+ */
+class ActionService extends \TYPO3\TestingFramework\Core\Functional\Framework\DataHandling\ActionService {}
