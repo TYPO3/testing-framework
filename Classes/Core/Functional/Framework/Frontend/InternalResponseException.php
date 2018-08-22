206a10c3ac48350fe56808144108611e8e824953
Oliver Hader <oliver@typo3.org>
2018-08-22 22:45:56 +0200

    [BUGFIX] Fallback to internal exception in case it cannot be created
    e.g. Doctrine\DBAL\Exception\DriverException cannot be reconstituted
    with just message and code, but requires additional context.

---
+++ Classes/Core/Functional/Framework/Frontend/InternalResponseException.php
@@ -0,0 +1,41 @@
+<?php
+namespace TYPO3\TestingFramework\Core\Functional\Framework\Frontend;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+class InternalResponseException extends \RuntimeException
+{
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @param string $message
+     * @param int $code
+     * @param string $type
+     */
+    public function __construct(string $message, int $code, string $type) {
+        parent::__construct($message, $code);
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+}
