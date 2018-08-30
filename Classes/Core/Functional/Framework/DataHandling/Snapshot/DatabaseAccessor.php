6c13dfeb2d88cb34a065909192d6238b55ee6066
Oliver Hader <oliver@typo3.org>
2018-08-30 09:11:45 +0200

    [BUGFIX] Export database binding types in snapshots

--- Classes/Core/Functional/Framework/DataHandling/Snapshot/DatabaseAccessor.php
+++ Classes/Core/Functional/Framework/DataHandling/Snapshot/DatabaseAccessor.php
@@ -16,6 +16,7 @@ namespace TYPO3\TestingFramework\Core\Functional\Framework\DataHandling\Snapshot
  */
 
 use Doctrine\DBAL\FetchMode;
+use Doctrine\DBAL\Schema\Table;
 use TYPO3\CMS\Core\Database\Connection;
 
 class DatabaseAccessor
@@ -38,21 +39,26 @@ class DatabaseAccessor
      */
     public function export(): array
     {
+        $schemaManager = $this->connection->getSchemaManager();
+
         $export = [];
-        $tableNames = array_filter(
-            $this->connection->getSchemaManager()->listTableNames(),
-            function (string $tableName) {
-                return stripos($tableName, 'cf_') !== 0;
+        foreach ($schemaManager->listTables() as $table) {
+            $tableName = $table->getName();
+            if (stripos($tableName, 'cf_') === 0) {
+                continue;
             }
-        );
-        foreach ($tableNames as $tableName) {
+
             $tableExport = $this->exportTable($tableName);
             if (empty($tableExport)) {
                 continue;
             }
+
             $export[] = [
                 'tableName' => $tableName,
-                'fieldNames' => array_keys($tableExport[0]),
+                'columns' => $this->prepareColumns(
+                    array_keys($tableExport[0]),
+                    $table
+                ),
                 'items' => array_map('array_values', $tableExport),
             ];
         }
@@ -70,7 +76,7 @@ class DatabaseAccessor
         foreach ($import as $tableImport) {
             $this->importTable(
                 $tableImport['tableName'] ?? '',
-                $tableImport['fieldNames'] ?? [],
+                $tableImport['columns'] ?? [],
                 $tableImport['items'] ?? []
             );
         }
@@ -91,14 +97,14 @@ class DatabaseAccessor
 
     /**
      * @param string $tableName
-     * @param array $fieldNames
+     * @param array $columns [columnName => columnType]
      * @param array $items
      *
      * @see \TYPO3\TestingFramework\Core\DatabaseConnectionWrapper handling IDENTITY_INSERT
      */
-    private function importTable(string $tableName, array $fieldNames, array $items)
+    private function importTable(string $tableName, array $columns, array $items)
     {
-        if (empty($tableName) || empty($fieldNames) || empty($items)) {
+        if (empty($tableName) || empty($columns) || empty($items)) {
             throw new \RuntimeException(
                 'Invalid import data',
                 1535487373
@@ -107,11 +113,32 @@ class DatabaseAccessor
 
         $this->connection->truncate($tableName);
 
+        $columnNames = array_keys($columns);
+        $columnTypes = array_values($columns);
         foreach ($items as $item) {
             $this->connection->insert(
                 $tableName,
-                array_combine($fieldNames, $item)
+                array_combine($columnNames, $item),
+                $columns
             );
         }
+    }
+
+    /**
+     * @param string[] $columnNames
+     * @param Table $table
+     * @return array
+     */
+    private function prepareColumns(array $columnNames, Table $table): array
+    {
+        $columnTypes = array_map(
+            function (string $columnName) use ($table) {
+                return $table->getColumn($columnName)
+                    ->getType()
+                    ->getBindingType();
+            },
+            $columnNames
+        );
+        return array_combine($columnNames, $columnTypes);
     }
 }
