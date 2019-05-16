f21004d2bfa8f756bc1cbf7b18afce64ac30b5fa
Andreas Fernandez <andreas.fernandez@typo3.com>
2019-05-16 10:44:05 +0200

    [BUGFIX] Only set initCommands on MySQL
    The command defined in `initCommands` only works with MySQL and MariaDB,
    thus this setting must not be set on any other DBMS.

--- Classes/Core/Functional/FunctionalTestCase.php
+++ Classes/Core/Functional/FunctionalTestCase.php
@@ -296,7 +296,9 @@ abstract class FunctionalTestCase extends BaseTestCase
                 $localConfiguration['DB']['Connections']['Default']['dbname'] = $dbName;
                 $localConfiguration['DB']['Connections']['Default']['wrapperClass'] = DatabaseConnectionWrapper::class;
                 $testbase->testDatabaseNameIsNotTooLong($originalDatabaseName, $localConfiguration);
-                $localConfiguration['DB']['Connections']['Default']['initCommands'] = 'SET SESSION sql_mode = \'STRICT_ALL_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_VALUE_ON_ZERO,NO_ENGINE_SUBSTITUTION,NO_ZERO_DATE,NO_ZERO_IN_DATE,ONLY_FULL_GROUP_BY\';';
+                if ($dbDriver === 'mysqli') {
+                    $localConfiguration['DB']['Connections']['Default']['initCommands'] = 'SET SESSION sql_mode = \'STRICT_ALL_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_VALUE_ON_ZERO,NO_ENGINE_SUBSTITUTION,NO_ZERO_DATE,NO_ZERO_IN_DATE,ONLY_FULL_GROUP_BY\';';
+                }
             } else {
                 $dbPath = $this->instancePath . '/test.sqlite';
                 $localConfiguration['DB']['Connections']['Default']['path'] = $dbPath;
