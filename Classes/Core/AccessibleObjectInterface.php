c20d66e7e9a1c6d85ee0ea2f5643ed5fd3dc10b5
Benni Mack <benni@typo3.org>
2019-12-01 17:22:00 +0100

    [!!!][TASK] Make AccessibleObject only work with call/get/set
    The other magic methods are not needed anymore.

--- Classes/Core/AccessibleObjectInterface.php
+++ Classes/Core/AccessibleObjectInterface.php
@@ -15,7 +15,7 @@ namespace TYPO3\TestingFramework\Core;
  */
 
 /**
- * This interface defines the methods provided by TYPO3\TestingFramework\Core\TestCase::getAccessibleMock.::
+ * This interface defines the methods provided by TYPO3\TestingFramework\Core\TestCase::getAccessibleMock.
  */
 interface AccessibleObjectInterface
 {
@@ -29,27 +29,6 @@ interface AccessibleObjectInterface
     public function _call($methodName);
 
     /**
-     * Calls the method $method without using call_user_func* and returns its return value.
-     *
-     * @param string $methodName name of method to call, must not be empty
-     * @param mixed &$arg1 first argument given to method $methodName
-     * @param mixed &$arg2 second argument given to method $methodName
-     * @param mixed &$arg3 third argument given to method $methodName
-     * @param mixed &$arg4 fourth argument given to method $methodName
-     * @param mixed &$arg5 fifth argument given to method $methodName
-     * @param mixed &$arg6 sixth argument given to method $methodName
-     * @param mixed &$arg7 seventh argument given to method $methodName
-     * @param mixed &$arg8 eighth argument given to method $methodName
-     * @param mixed &$arg9 ninth argument given to method $methodName
-     *
-     * @return mixed the return value from the method $methodName
-     */
-    public function _callRef(
-        $methodName, &$arg1 = null, &$arg2 = null, &$arg3 = null, &$arg4 = null, &$arg5= null, &$arg6 = null, &$arg7 = null,
-        &$arg8 = null, &$arg9 = null
-    );
-
-    /**
      * Sets the value of a property.
      *
      * @param string $propertyName name of property to set value for, must not be empty
@@ -60,26 +39,6 @@ interface AccessibleObjectInterface
     public function _set($propertyName, $value);
 
     /**
-     * Sets the value of a property by reference.
-     *
-     * @param string $propertyName name of property to set value for, must not be empty
-     * @param mixed &$value the new value for the property defined in $propertyName
-     *
-     * @return void
-     */
-    public function _setRef($propertyName, &$value);
-
-    /**
-     * Sets the value of a static property.
-     *
-     * @param string $propertyName name of property to set value for, must not be empty
-     * @param mixed $value the new value for the property defined in $propertyName
-     *
-     * @return void
-     */
-    public function _setStatic($propertyName, $value);
-
-    /**
      * Gets the value of the given property.
      *
      * @param string $propertyName name of property to return value of, must not be empty
@@ -88,12 +47,4 @@ interface AccessibleObjectInterface
      */
     public function _get($propertyName);
 
-    /**
-     * Gets the value of the given static property.
-     *
-     * @param string $propertyName name of property to return value of, must not be empty
-     *
-     * @return mixed the value of the static property $propertyName
-     */
-    public function _getStatic($propertyName);
 }
