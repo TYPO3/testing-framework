TYPO3/testing-framework

commit 207e6f212bb2f56d65b0f7d1aaf6104059c1d413

[TASK] Only truncate MySQL tables that have been touched (#262)
"Touched" means "the auto increment has been changed (if there is
one)" or "there are some records in it".

This massively speeds up functional tests (as the table truncation
usually is the slowest part of the test setup).

Classes/Core/Testbase.php
@@ -16,6 +16,7 @@ namespace TYPO3\TestingFramework\Core;
 
 use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Platforms\MySqlPlatform;
 use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
 use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Doctrine\DBAL\Platforms\SQLServerPlatform;
@@ -619,16 +620,69 @@ class Testbase
     }
 
     /**
-     * Truncate all tables.
-     * For functional and acceptance tests.
+     * Truncates all tables.
+     *
+     * For functional tests.
      *
      * @throws Exception
-     * @return void
      */
     public function initializeTestDatabaseAndTruncateTables(): void
     {
+        /** @var Connection $connection */
         $connection = GeneralUtility::makeInstance(ConnectionPool::class)
             ->getConnectionByName(ConnectionPool::DEFAULT_CONNECTION_NAME);
+
+        $platform = $connection->getDatabasePlatform();
+        if ($platform instanceof MySqlPlatform) {
+            $this->truncateAllTablesForMysql();
+        } else {
+            // @todo: Optimize truncation for other platforms, too.
+            $this->truncateAllTablesForOtherDatabases();
+        }
+    }
+
+    /**
+     * Truncates all tables for MySQL databases in an optimized way.
+     *
+     * This method tries to avoid the (expensive) truncate if possible:
+     * - If the table has an auto-increment value (which usually is the `uid` column`) and that value has changed,
+     *   this method will truncate the table.
+     * - If the table does not have an auto-increment value, but it has at least one row (where the exact number does
+     *   not matter), this method will truncate the table.
+     * - Otherwise, this method will skip the truncate. (For tables without an auto-increment value, this means that
+     *   the table either has not been touched at all beforehand, or that all records have already been deleted).
+     */
+    private function truncateAllTablesForMysql(): void
+    {
+        /** @var Connection $connection */
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionByName(ConnectionPool::DEFAULT_CONNECTION_NAME);
+
+        $databaseName = $connection->getDatabase();
+        $query = "SHOW TABLE STATUS FROM $databaseName";
+        // @todo: Switch to fetchAllAssociative() when core v10 compat is dropped.
+        $result = $connection->executeQuery($query)->fetchAll();
+        foreach ($result as $tableData) {
+            $hasChangedAutoIncrement = ((int)$tableData['Auto_increment']) > 1;
+            $hasAtLeastOneRow = ((int)$tableData['Rows']) > 0;
+            $isChanged = $hasChangedAutoIncrement || $hasAtLeastOneRow;
+            if ($isChanged) {
+                $tableName = $tableData['Name'];
+                $connection->truncate($tableName);
+                self::resetTableSequences($connection, $tableName);
+            }
+        }
+    }
+
+    /**
+     * Truncates all tables without any database-specific optimizations.
+     */
+    private function truncateAllTablesForOtherDatabases(): void
+    {
+        /** @var Connection $connection */
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionByName(ConnectionPool::DEFAULT_CONNECTION_NAME);
+
         $schemaManager = $connection->getSchemaManager();
         foreach ($schemaManager->listTables() as $table) {
             $connection->truncate($table->getName());

commit 7225df015eccdda13df49454b8d9d8f15e8030ad

[TASK] Only truncate touched MySQL tables using real row counts
This is a follow up to mitigate reliable issues for mysql introduced by #262 .

Using information schema for row counts is not reliable enough for innodb tables, and for mysql 8.0 it fail hard. 

Using subquery as a joinable table building information if tables has at least one row mitigates this issue.

Classes/Core/Testbase.php
@@ -659,17 +659,49 @@ class Testbase
             ->getConnectionByName(ConnectionPool::DEFAULT_CONNECTION_NAME);
 
         $databaseName = $connection->getDatabase();
-        $query = "SHOW TABLE STATUS FROM $databaseName";
+        $tableNames = $connection->getSchemaManager()->listTableNames();
+
+        // no tables to process;
+        if (!$tableNames) {
+            return;
+        }
+
+        // build subselect to get joinable table with information if table has at least one row.
+        // This is needed because information_schema.table_rows is not reliable enough for innodb engine.
+        // see https://dev.mysql.com/doc/mysql-infoschema-excerpt/5.7/en/information-schema-tables-table.html TABLE_ROWS
+        $fromTableUnionSubSelectQuery = [];
+        foreach($tableNames as $tableName) {
+            $fromTableUnionSubSelectQuery[] = sprintf(
+                ' select %s as table_name, exists(select * from %s limit 1) as has_rows',
+                $connection->quote($tableName),
+                $connection->quoteIdentifier($tableName)
+            );
+        }
+        $fromTableUnionSubSelectQuery = implode(' UNION ', $fromTableUnionSubSelectQuery);
+
+        $query = sprintf(
+            "
+                select
+                    table_real_rowcounts.*,
+                    information_schema.tables.AUTO_INCREMENT as auto_increment
+                from (%s) as table_real_rowcounts
+                inner join information_schema.tables ON (
+                        information_schema.tables.TABLE_SCHEMA = %s
+                    and information_schema.tables.TABLE_NAME = table_real_rowcounts.table_name
+                )
+                ",
+            $fromTableUnionSubSelectQuery,
+            $connection->quote($databaseName)
+        );
         // @todo: Switch to fetchAllAssociative() when core v10 compat is dropped.
         $result = $connection->executeQuery($query)->fetchAll();
-        foreach ($result as $tableData) {
-            $hasChangedAutoIncrement = ((int)$tableData['Auto_increment']) > 1;
-            $hasAtLeastOneRow = ((int)$tableData['Rows']) > 0;
+        foreach($result as $tableData) {
+            $hasChangedAutoIncrement = ((int)$tableData['auto_increment']) > 1;
+            $hasAtLeastOneRow = (bool)$tableData['has_rows'];
             $isChanged = $hasChangedAutoIncrement || $hasAtLeastOneRow;
             if ($isChanged) {
-                $tableName = $tableData['Name'];
+                $tableName = $tableData['table_name'];
                 $connection->truncate($tableName);
-                self::resetTableSequences($connection, $tableName);
             }
         }
     }