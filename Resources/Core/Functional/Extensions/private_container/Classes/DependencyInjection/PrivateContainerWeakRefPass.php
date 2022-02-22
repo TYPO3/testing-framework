662e6517eb0aae34e6e8e1c228bdf21e17dead89
Benjamin Franzke <bfr@qbus.de>
2022-02-22 15:34:35 +0100

    [FEATURE] Provide access to private services in functional tests
    Functional tests frequently need to instantiate $subject
    or a dependency. This can be cumbersome if the service
    in question is not public, since $this->getContainer()->get()
    can currently only instantiate services marked as public.
    
    The patch adds compiler passes as always-registered core
    extension to DI in functional tests that allows to get()
    private containers, too.
    
    FunctionalTestCase provides access to both, public and
    private services via the method `get()`
    (as per PSR-11 ContainerInterface). FunctionalTestCase
    now implements ContainerInterface in order for static
    code analysers like phpstan will be able to resolve
    types returned by $this->get(ServiceName::class).
    
    This methods should only be used in the test case
    itself. Whenever a service requires a ContainerInterface
    argument, the default container (containing only public services)
    should be obtained via $this->getContainer() and injected:
    Injecting $this as general purpose ContainerInterface implementation
    may shadow bugs when a `public: true` definition is
    missing in the tested code.
    
    Based on:
    https://github.com/symfony/symfony/blob/6.1/src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/TestServiceContainerWeakRefPass.php
    https://github.com/symfony/symfony/blob/6.1/src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/TestServiceContainerRealRefPass.php
    https://github.com/symfony/symfony/blob/6.1/src/Symfony/Bundle/FrameworkBundle/Test/TestContainer.php
    
    Related change for TYPO3 core:
    https://review.typo3.org/c/Packages/TYPO3.CMS/+/73646
    
    Releases: main, v7, v6

---
+++ Resources/Core/Functional/Extensions/private_container/Classes/DependencyInjection/PrivateContainerWeakRefPass.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\PrivateContainer\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\ServiceLocatorTagPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Register used private services. This is a special testing-framework
+ * functional service pass to allow $this->get() of private services in
+ * functional tests.
+ */
+class PrivateContainerWeakRefPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $privateServices = [];
+        $definitions = $container->getDefinitions();
+
+        foreach ($definitions as $id => $definition) {
+            if ($id &&
+                $id[0] !== '.' &&
+                (!$definition->isPublic() || $definition->isPrivate() || $definition->hasTag('container.private')) &&
+                !$definition->hasErrors() &&
+                !$definition->isAbstract()
+            ) {
+                $privateServices[$id] = new Reference($id, ContainerBuilder::IGNORE_ON_UNINITIALIZED_REFERENCE);
+            }
+        }
+
+        $aliases = $container->getAliases();
+
+        foreach ($aliases as $id => $alias) {
+            if ($id && $id[0] !== '.' && (!$alias->isPublic() || $alias->isPrivate())) {
+                while (isset($aliases[$target = (string) $alias])) {
+                    $alias = $aliases[$target];
+                }
+                if (isset($definitions[$target]) && !$definitions[$target]->hasErrors() && !$definitions[$target]->isAbstract()) {
+                    $privateServices[$id] = new Reference($target, ContainerBuilder::IGNORE_ON_UNINITIALIZED_REFERENCE);
+                }
+            }
+        }
+
+        if ($privateServices) {
+            $id = (string) ServiceLocatorTagPass::register($container, $privateServices);
+            $container->setDefinition('typo3.testing-framework.private-container', $container->getDefinition($id))->setPublic(true);
+            $container->removeDefinition($id);
+        }
+    }
+}
