82eb016fff476ffb0c7e8c6ac39b72832e253043
Benjamin Franzke <bfr@qbus.de>
2022-02-22 15:34:35 +0100

    [FEATURE] Provide access to private services in functional tests
    Functional tests frequently need to instantiate $subject
    or a dependency. This can be cumbersome if the service
    in question is not public, since $this->getContainer()->get()
    can currently only instantiate services marked as public.
    
    The patch adds compiler passes as always-registered core
    extension to DI in functional tests that allows to get()
    private containers, too.
    
    FunctionalTestCase provides access to both, public and
    private services via the method `get()`
    (as per PSR-11 ContainerInterface). FunctionalTestCase
    now implements ContainerInterface in order for static
    code analysers like phpstan will be able to resolve
    types returned by $this->get(ServiceName::class).
    
    This methods should only be used in the test case
    itself. Whenever a service requires a ContainerInterface
    argument, the default container (containing only public services)
    should be obtained via $this->getContainer() and injected:
    Injecting $this as general purpose ContainerInterface implementation
    may shadow bugs when a `public: true` definition is
    missing in the tested code.
    
    Based on:
    https://github.com/symfony/symfony/blob/6.1/src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/TestServiceContainerWeakRefPass.php
    https://github.com/symfony/symfony/blob/6.1/src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/TestServiceContainerRealRefPass.php
    https://github.com/symfony/symfony/blob/6.1/src/Symfony/Bundle/FrameworkBundle/Test/TestContainer.php
    
    Related change for TYPO3 core:
    https://review.typo3.org/c/Packages/TYPO3.CMS/+/73646
    
    Releases: main, v7, v6

---
+++ Resources/Core/Functional/Extensions/private_container/Classes/DependencyInjection/PrivateContainerRealRefPass.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\PrivateContainer\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Unset unused private services. This is used in functional tests to
+ * only add private services that are actually used.
+ */
+class PrivateContainerRealRefPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('typo3.testing-framework.private-container')) {
+            return;
+        }
+
+        $privateContainer = $container->getDefinition('typo3.testing-framework.private-container');
+        $definitions = $container->getDefinitions();
+        $privateServices = $privateContainer->getArgument(0);
+
+        foreach ($privateServices as $id => $argument) {
+            if (isset($definitions[$target = (string) $argument->getValues()[0]])) {
+                $argument->setValues([new Reference($target)]);
+            } else {
+                unset($privateServices[$id]);
+            }
+        }
+
+        $privateContainer->replaceArgument(0, $privateServices);
+    }
+}
---
+++ Resources/Core/Functional/Extensions/private_container/ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Private Container',
+    'description' => 'Private Container',
+    'version' => '1.0.0',
+    'state' => 'stable',
+    'createDirs' => '',
+    'author' => 'Benjamin Franzke',
+    'author_email' => 'bfr@qbus.de',
+    'author_company' => '',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.0.0-12.99.99'
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
