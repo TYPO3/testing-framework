0126b33f2d3f29b634b2d8a8a0074be05313b4cf
Stefan Bürk <stefan.buerk@impactmedia.de>
2022-02-27 18:32:02 +0100

    [TASK] Add phpstan constants bootstrap file and raise level 1
    This change adds a corresponding phpstand bootstrap file to
    help phpstan with resolving constants properly. Also raises
    level to level 1.

---
+++ Build/phpstan/phpstan-constants.php
@@ -0,0 +1,7 @@
+<?php
+
+define('LF', chr(10));
+
+// testing-framework defines this, used in various tests.
+// @todo Maybe this is wrong. Reinvestigate and fix later if this gets a problem.
+define('ORIGINAL_ROOT', dirname(__FILE__, 2) . '/');
