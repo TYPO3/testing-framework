583d4c3557e01324922c15c6072dfaacf769b6fb
Stefan Bürk <stefan.buerk@impactmedia.de>
2022-02-27 19:48:23 +0100

    [TASK] Use proper type hints for set_error_handler() dummy callback
    Solve phpstan baseline entry for set_error_handler() with adding
    proper type hints to used dummy closure callback method and make
    phpstan happier.

--- Classes/Core/Functional/FunctionalTestCase.php
+++ Classes/Core/Functional/FunctionalTestCase.php
@@ -415,7 +415,7 @@ abstract class FunctionalTestCase extends BaseTestCase implements ContainerInter
             // fails to unset/restore it's custom error handler as "risky".
             // @todo: Consider moving this to BaseTestCase to have it for unit tests, too.
             // @see: https://github.com/sebastianbergmann/phpunit/issues/4801
-            $previousErrorHandler = set_error_handler(function () {});
+            $previousErrorHandler = set_error_handler(function (int $errorNumber, string $errorString, string $errorFile, int $errorLine): bool {return false;});
             restore_error_handler();
             if (!$previousErrorHandler instanceof ErrorHandler) {
                 throw new RiskyTestError(
