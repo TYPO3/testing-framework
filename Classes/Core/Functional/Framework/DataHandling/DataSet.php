268b5794ca395ec5400e20a6ec7814d1f5787b7b
Christian Kuhn <lolli@schwarzbu.ch>
2022-07-17 15:57:02 +0200

    [TASK] Improve comments on .csv related DataSet worker class (#385)

--- Classes/Core/Functional/Framework/DataHandling/DataSet.php
+++ Classes/Core/Functional/Framework/DataHandling/DataSet.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-namespace TYPO3\TestingFramework\Core\Functional\Framework\DataHandling;
-
 /*
  * This file is part of the TYPO3 CMS project.
  *
@@ -17,43 +15,127 @@ namespace TYPO3\TestingFramework\Core\Functional\Framework\DataHandling;
  * The TYPO3 project - inspiring people to share!
  */
 
+namespace TYPO3\TestingFramework\Core\Functional\Framework\DataHandling;
+
 /**
- * DataHandler DataSet
+ * Internal worker class managing .csv fixture file contents.
+ *
+ * This is used in functional and acceptance tests to load .csv fixture files
+ * into database and to assert current database state with .csv fixture files.
+ *
+ * A .csv file can include table rows from multiple tables.
+ * When importing .csv files to database, "default" values from TCA for this table are applied.
+ *
+ * A typical example .csv file looks like this, this specifies two rows of
+ * table "pages" and two rows to table "tt_content". The fields have to
+ * exist in the table:
+ *
+ * "pages"
+ * ,"uid","pid","sorting","deleted","t3_origuid","t3ver_wsid","t3ver_state","t3ver_stage","t3ver_oid","title"
+ * ,1,0,256,0,0,0,0,0,0,"Root Page"
+ * ,2,0,256,0,0,0,0,0,0,"Another Page"
+ * "tt_content"
+ * ,"uid","pid","sorting","deleted","sys_language_uid","l18n_parent","t3_origuid","t3ver_wsid","t3ver_state","t3ver_stage","t3ver_oid","header"
+ * ,1,1,256,0,0,0,0,0,0,0,0,"Regular Element #1"
+ * ,1,1,512,0,0,0,0,0,0,0,0,"Regular Element #2"
+ *
+ * @internal Directly using this class is discouraged, it may change any time.
+ *           Use API methods like importCSVDataSet() (in functional & acceptance tests)
+ *           and assertCSVDataSet() (in functional tests) instead.
  */
-class DataSet
+final class DataSet
 {
-    /**
-     * @var array
-     */
-    protected $data;
+    private array $data;
 
     /**
-     * @param string $fileName
-     * @param bool $applyDefaultValues
-     * @return DataSet
+     * Private constructor: An instance of this class is returned using DataSet::read()
      */
-    public static function read(string $fileName, bool $applyDefaultValues = false): DataSet
+    private function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Main entry method: Get at absosulete (!) path to a .csv file, read it and return an instance of self
+     */
+    public static function read(string $fileName, bool $applyDefaultValues = false): self
     {
         $data = self::parseData(self::readData($fileName));
-
         if ($applyDefaultValues) {
             $data = self::applyDefaultValues($data);
         }
-
         return new DataSet($data);
     }
 
     /**
-     * @param string $fileName
-     * @return array
-     * @throws \RuntimeException
+     * Get the list of table names included in a loaded .csv file
      */
-    protected static function readData(string $fileName): array
+    public function getTableNames(): array
+    {
+        return array_keys($this->data);
+    }
+
+    /**
+     * Get the list of fields of a single table included in a loaded .csv file
+     */
+    public function getFields(string $tableName): ?array
+    {
+        $fields = null;
+        if (isset($this->data[$tableName]['fields'])) {
+            $fields = $this->data[$tableName]['fields'];
+        }
+        return $fields;
+    }
+
+    /**
+     * When a table has a "uid" field as primary key, this method
+     * return this field array key from getFields(). This is set when a
+     * table has a column named "uid".
+     */
+    public function getIdIndex(string $tableName): ?int
+    {
+        $idIndex = null;
+        if (isset($this->data[$tableName]['idIndex'])) {
+            $idIndex = $this->data[$tableName]['idIndex'];
+        }
+        return $idIndex;
+    }
+
+    /**
+     * When a table has a "hash" field as primary key, this method
+     * return this field array key from getFields(). This is set when a
+     * table has a column named "hash".
+     */
+    public function getHashIndex(string $tableName): ?int
+    {
+        $hashIndex = null;
+        if (isset($this->data[$tableName]['hashIndex'])) {
+            $hashIndex = $this->data[$tableName]['hashIndex'];
+        }
+        return $hashIndex;
+    }
+
+    /**
+     * Return a list of rows of given table. Keys are the uid or hash
+     * index fields of that table.
+     */
+    public function getElements(string $tableName): ?array
+    {
+        $elements = null;
+        if (isset($this->data[$tableName]['elements'])) {
+            $elements = $this->data[$tableName]['elements'];
+        }
+        return $elements;
+    }
+
+    /**
+     * Read a file and return an array of all lines as csv parsed array
+     */
+    private static function readData(string $fileName): array
     {
         if (!file_exists($fileName)) {
             throw new \RuntimeException('File "' . $fileName . '" does not exist', 1476049619);
         }
-
         $rawData = [];
         $fileHandle = fopen($fileName, 'r');
         // UTF-8 Files starting with BOM will break the first field in the first line
@@ -71,15 +153,12 @@ class DataSet
     }
 
     /**
-     * Parses CSV data.
+     * Parse lines of the CSV array to a 'table' and 'table-row' structure.
      *
-     * Special values are:
+     * Special value treatment:
      * + "\NULL" to treat as NULL value
-     *
-     * @param array $rawData
-     * @return array
      */
-    protected static function parseData(array $rawData): array
+    private static function parseData(array $rawData): array
     {
         $data = [];
         $tableName = null;
@@ -150,21 +229,16 @@ class DataSet
     }
 
     /**
-     * Applies TCA default values to missing fields on the imported scenario data-set.
+     * Apply TCA default values to missing fields on the imported scenario data-set.
      * This is basically required for running the functional tests in a SQL strict mode environment.
-     *
-     * @param array $data
-     * @return array
      */
-    protected static function applyDefaultValues(array $data): array
+    private static function applyDefaultValues(array $data): array
     {
         foreach ($data as $tableName => $sections) {
             if (empty($GLOBALS['TCA'][$tableName]['columns'])) {
                 continue;
             }
-
             $fields = $sections['fields'];
-
             foreach ($GLOBALS['TCA'][$tableName]['columns'] as $tcaFieldName => $tcaFieldConfiguration) {
                 // Skip if field was already imported
                 if (in_array($tcaFieldName, $fields)) {
@@ -178,7 +252,6 @@ class DataSet
                 if (empty($tcaFieldConfiguration['config']) || !array_key_exists('default', $tcaFieldConfiguration['config'])) {
                     continue;
                 }
-
                 $data[$tableName]['fields'][] = $tcaFieldName;
                 foreach ($data[$tableName]['elements'] as &$element) {
                     $element[$tcaFieldName] = $tcaFieldConfiguration['config']['default'];
@@ -186,73 +259,5 @@ class DataSet
             }
         }
         return $data;
-    }
-
-    /**
-     * @param array $data
-     */
-    public function __construct(array $data)
-    {
-        $this->data = $data;
-    }
-
-    /**
-     * @return array
-     */
-    public function getTableNames(): array
-    {
-        return array_keys($this->data);
-    }
-
-    /**
-     * @param string $tableName
-     * @return array|null
-     */
-    public function getFields(string $tableName)
-    {
-        $fields = null;
-        if (isset($this->data[$tableName]['fields'])) {
-            $fields = $this->data[$tableName]['fields'];
-        }
-        return $fields;
-    }
-
-    /**
-     * @param string $tableName
-     * @return int|null
-     */
-    public function getIdIndex(string $tableName)
-    {
-        $idIndex = null;
-        if (isset($this->data[$tableName]['idIndex'])) {
-            $idIndex = $this->data[$tableName]['idIndex'];
-        }
-        return $idIndex;
-    }
-
-    /**
-     * @param string $tableName
-     * @return int|null
-     */
-    public function getHashIndex(string $tableName): ?int
-    {
-        $hashIndex = null;
-        if (isset($this->data[$tableName]['hashIndex'])) {
-            $hashIndex = $this->data[$tableName]['hashIndex'];
-        }
-        return $hashIndex;
-    }
-
-    /**
-     * @param string $tableName
-     * @return array|null
-     */
-    public function getElements(string $tableName)
-    {
-        $elements = null;
-        if (isset($this->data[$tableName]['elements'])) {
-            $elements = $this->data[$tableName]['elements'];
-        }
-        return $elements;
     }
 }
