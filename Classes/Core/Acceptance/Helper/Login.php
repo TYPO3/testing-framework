760a498581d4329cf2fb73dd6b8e4d946a75451d
Christian Kuhn <lolli@schwarzbu.ch>
2022-10-26 13:05:01 +0200

    [TASK] Use acceptance test loadSessionSnapshot without debug
    Not debugging reset cookies reduces noise
    running acceptance tests. This has been added
    with codeception/module-webdriver 3.2.0.
    
    https://github.com/Codeception/module-webdriver/pull/111
    https://github.com/Codeception/module-webdriver/issues/110

--- Classes/Core/Acceptance/Helper/Login.php
+++ Classes/Core/Acceptance/Helper/Login.php
@@ -102,7 +102,7 @@ class Login extends Module
      */
     public function _loadSession()
     {
-        return $this->getWebDriver()->loadSessionSnapshot('login');
+        return $this->getWebDriver()->loadSessionSnapshot('login', false);
     }
 
     public function _deleteSession()
