5c02f49f19471218348cb7e5cf494a3ae8b5da47
DavidBruchmann <david.bruchmann@gmail.com>
2023-04-23 13:56:34 +0700

    remove usage of

--- Classes/Composer/ComposerPackageInfo.php
+++ Classes/Composer/ComposerPackageInfo.php
@@ -188,6 +188,6 @@ class ComposerPackageInfo extends \Composer\InstalledVersions
 
     public static function getAutoload()
     {
-        return !empty($_composer_autoload_path) ? $_composer_autoload_path : realpath(InstalledVersions::getInstallPath('typo3/cms-core') . '/../../autoload.php');
+        return realpath(InstalledVersions::getInstallPath('typo3/cms-core') . '/../../autoload.php');
     }
 }
