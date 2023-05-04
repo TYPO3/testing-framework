98d4f43130b1154eeba8aa040d53c4cf5a2c89c5
Stefan Bürk <stefan@buerk.tech>
2023-05-04 03:07:29 +0200

    [TASK] Avoid direct "symfony/filesystem" dependency
    Package "symfony/filesystem" is not a direct dependency,
    but recently added "ComposerPackageManager" introduced
    usage by using the "Path::canonicalize()" method of it.
    
    Direct external dependencies should be kept as minimal
    as possible. However, we do not need the full class and
    full ability, thus implementing a subset of the method
    directly along with test coverage.
    
    This change provides an alternative approch then #463.
    
    Releases: main, 7
    Related: #463
    Related: #434
    Related: #435

---
+++ Tests/Unit/Composer/ComposerPackageManagerTest.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TYPO3\TestingFramework\Tests\Unit\Composer;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\TestingFramework\Composer\ComposerPackageManager;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class ComposerPackageManagerTest extends UnitTestCase
+{
+    public static function sanitizePathReturnsExpectedValueDataProvider(): \Generator
+    {
+        // relative paths (forward slash)
+        yield ['css/./style.css', 'css/style.css'];
+        yield ['css/../style.css', 'style.css'];
+        yield ['css/./../style.css', 'style.css'];
+        yield ['css/.././style.css', 'style.css'];
+        yield ['css/../../style.css', '../style.css'];
+        yield ['./css/style.css', 'css/style.css'];
+        yield ['../css/style.css', '../css/style.css'];
+        yield ['./../css/style.css', '../css/style.css'];
+        yield ['.././css/style.css', '../css/style.css'];
+        yield ['../../css/style.css', '../../css/style.css'];
+        yield ['', ''];
+        yield ['.', ''];
+        yield ['..', '..'];
+        yield ['./..', '..'];
+        yield ['../.', '..'];
+        yield ['../..', '../..'];
+
+        // relative paths (backslash)
+        yield ['css\\.\\style.css', 'css/style.css'];
+        yield ['css\\..\\style.css', 'style.css'];
+        yield ['css\\.\\..\\style.css', 'style.css'];
+        yield ['css\\..\\.\\style.css', 'style.css'];
+        yield ['css\\..\\..\\style.css', '../style.css'];
+        yield ['.\\css\\style.css', 'css/style.css'];
+        yield ['..\\css\\style.css', '../css/style.css'];
+        yield ['.\\..\\css\\style.css', '../css/style.css'];
+        yield ['..\\.\\css\\style.css', '../css/style.css'];
+        yield ['..\\..\\css\\style.css', '../../css/style.css'];
+
+        // absolute paths (forward slash, UNIX)
+        yield ['/css/style.css', '/css/style.css'];
+        yield ['/css/./style.css', '/css/style.css'];
+        yield ['/css/../style.css', '/style.css'];
+        yield ['/css/./../style.css', '/style.css'];
+        yield ['/css/.././style.css', '/style.css'];
+        yield ['/./css/style.css', '/css/style.css'];
+        yield ['/../css/style.css', '/css/style.css'];
+        yield ['/./../css/style.css', '/css/style.css'];
+        yield ['/.././css/style.css', '/css/style.css'];
+        yield ['/../../css/style.css', '/css/style.css'];
+
+        // absolute paths (backslash, UNIX)
+        yield ['\\css\\style.css', '/css/style.css'];
+        yield ['\\css\\.\\style.css', '/css/style.css'];
+        yield ['\\css\\..\\style.css', '/style.css'];
+        yield ['\\css\\.\\..\\style.css', '/style.css'];
+        yield ['\\css\\..\\.\\style.css', '/style.css'];
+        yield ['\\.\\css\\style.css', '/css/style.css'];
+        yield ['\\..\\css\\style.css', '/css/style.css'];
+        yield ['\\.\\..\\css\\style.css', '/css/style.css'];
+        yield ['\\..\\.\\css\\style.css', '/css/style.css'];
+        yield ['\\..\\..\\css\\style.css', '/css/style.css'];
+
+        // absolute paths (forward slash, Windows)
+        yield ['C:/css/style.css', 'C:/css/style.css'];
+        yield ['C:/css/./style.css', 'C:/css/style.css'];
+        yield ['C:/css/../style.css', 'C:/style.css'];
+        yield ['C:/css/./../style.css', 'C:/style.css'];
+        yield ['C:/css/.././style.css', 'C:/style.css'];
+        yield ['C:/./css/style.css', 'C:/css/style.css'];
+        yield ['C:/../css/style.css', 'C:/css/style.css'];
+        yield ['C:/./../css/style.css', 'C:/css/style.css'];
+        yield ['C:/.././css/style.css', 'C:/css/style.css'];
+        yield ['C:/../../css/style.css', 'C:/css/style.css'];
+
+        // absolute paths (backslash, Windows)
+        yield ['C:\\css\\style.css', 'C:/css/style.css'];
+        yield ['C:\\css\\.\\style.css', 'C:/css/style.css'];
+        yield ['C:\\css\\..\\style.css', 'C:/style.css'];
+        yield ['C:\\css\\.\\..\\style.css', 'C:/style.css'];
+        yield ['C:\\css\\..\\.\\style.css', 'C:/style.css'];
+        yield ['C:\\.\\css\\style.css', 'C:/css/style.css'];
+        yield ['C:\\..\\css\\style.css', 'C:/css/style.css'];
+        yield ['C:\\.\\..\\css\\style.css', 'C:/css/style.css'];
+        yield ['C:\\..\\.\\css\\style.css', 'C:/css/style.css'];
+        yield ['C:\\..\\..\\css\\style.css', 'C:/css/style.css'];
+
+        // Windows special case
+        yield ['C:', 'C:/'];
+
+        // Don't change malformed path
+        yield ['C:css/style.css', 'C:css/style.css'];
+
+        // absolute paths (stream, UNIX)
+        yield ['phar:///css/style.css', 'phar:///css/style.css'];
+        yield ['phar:///css/./style.css', 'phar:///css/style.css'];
+        yield ['phar:///css/../style.css', 'phar:///style.css'];
+        yield ['phar:///css/./../style.css', 'phar:///style.css'];
+        yield ['phar:///css/.././style.css', 'phar:///style.css'];
+        yield ['phar:///./css/style.css', 'phar:///css/style.css'];
+        yield ['phar:///../css/style.css', 'phar:///css/style.css'];
+        yield ['phar:///./../css/style.css', 'phar:///css/style.css'];
+        yield ['phar:///.././css/style.css', 'phar:///css/style.css'];
+        yield ['phar:///../../css/style.css', 'phar:///css/style.css'];
+
+        // absolute paths (stream, Windows)
+        yield ['phar://C:/css/style.css', 'phar://C:/css/style.css'];
+        yield ['phar://C:/css/./style.css', 'phar://C:/css/style.css'];
+        yield ['phar://C:/css/../style.css', 'phar://C:/style.css'];
+        yield ['phar://C:/css/./../style.css', 'phar://C:/style.css'];
+        yield ['phar://C:/css/.././style.css', 'phar://C:/style.css'];
+        yield ['phar://C:/./css/style.css', 'phar://C:/css/style.css'];
+        yield ['phar://C:/../css/style.css', 'phar://C:/css/style.css'];
+        yield ['phar://C:/./../css/style.css', 'phar://C:/css/style.css'];
+        yield ['phar://C:/.././css/style.css', 'phar://C:/css/style.css'];
+        yield ['phar://C:/../../css/style.css', 'phar://C:/css/style.css'];
+    }
+
+    /**
+     * @test
+     * @dataProvider sanitizePathReturnsExpectedValueDataProvider
+     */
+    public function sanitizePathReturnsExpectedValue(string $path, string $expectedPath): void
+    {
+        $subject = new ComposerPackageManager();
+        self::assertSame($expectedPath, $subject->sanitizePath($path));
+    }
+}
