6def33861b162601491caef6852471d0af5f9bae
Christian Kuhn <lolli@schwarzbu.ch>
2024-02-06 14:17:34 +0100

    [TASK] Happify phpstan again
    It seems that `@phpstan-ignore-next-line` mumbles when
    it does non actively suppress something. With the release
    of doctrine 4, this kicks in for two lines which phpstan
    gets right now.
    
    phpstan with PHP 8.2 now collides with 8.1, we skip the 8.1
    run. Also add PHP 8.3 to the matrix.

--- Classes/Core/Functional/Framework/DataHandling/Snapshot/DatabaseSnapshot.php
+++ Classes/Core/Functional/Framework/DataHandling/Snapshot/DatabaseSnapshot.php
@@ -102,15 +102,12 @@ class DatabaseSnapshot
     }
 
     /**
-     * @todo Replace usages by direct instanceof checks when TYPO3 v13.0 / Doctrine DBAL 4 is lowes supported version.
-     *
-     * @param Connection $connection
-     * @return bool
+     * @todo Replace usages by direct instanceof checks when
+     *       TYPO3 v13.0 / Doctrine DBAL 4 is lowest supported version.
      * @throws \Doctrine\DBAL\Exception
      */
     private function isSQLite(Connection $connection): bool
     {
-        /** @phpstan-ignore-next-line */
         return $connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SQLitePlatform;
     }
 }
