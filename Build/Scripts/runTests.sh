3c758b0aab75f3cf56c3f418aa6d42335cf24e0b
Christian Kuhn <lolli@schwarzbu.ch>
2024-08-10 13:36:51 +0200

    [TASK] Add PHP 8.4 to test matrix (#610) (#611)

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -44,6 +44,7 @@ Options:
             - 8.1 (default): use PHP 8.1
             - 8.2: use PHP 8.2
             - 8.3: use PHP 8.3
+            - 8.4: use PHP 8.4
 
     -x
         Only with -s cgl|unit
@@ -117,7 +118,7 @@ while getopts ":b:s:p:hxn" OPT; do
             ;;
         p)
             PHP_VERSION=${OPTARG}
-            if ! [[ ${PHP_VERSION} =~ ^(8.1|8.2|8.3)$ ]]; then
+            if ! [[ ${PHP_VERSION} =~ ^(8.1|8.2|8.3|8.4)$ ]]; then
                 INVALID_OPTIONS+=("${OPTARG}")
             fi
             ;;
