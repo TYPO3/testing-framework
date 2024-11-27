a6aa622fd7639fd535f3edb5ac9bc83323671939
Stefan Bürk <stefan@buerk.tech>
2024-11-27 17:59:12 +0100

    [TASK] Add missing `composer.json` to testing-framework extensions
    The typo3/testing-framework provides two TYPO3 extensions,
    which are required and bound to every created functional
    test instance.
    
    Adding missing `composer.json` for these two extensions.
    
    Releases: main, 8

--- Resources/Core/Functional/Extensions/private_container/ext_emconf.php
+++ Resources/Core/Functional/Extensions/private_container/ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-12.99.99',
+            'typo3' => '13.0.0-14.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
