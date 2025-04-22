23966b638881fd8756bb7c4400801c1d2642b2ae
Benni Mack <benni@typo3.org>
2025-04-22 13:56:30 +0200

    [TASK] Improve error message (#671)
    When a versioned record cannot be found, the
    DB table and the UID of the live record is now
    added to the message of the exception.

--- Classes/Core/Functional/Framework/DataHandling/ActionService.php
+++ Classes/Core/Functional/Framework/DataHandling/ActionService.php
@@ -430,7 +430,7 @@ class ActionService
                 $versionedUid = $this->getVersionedId($tableName, $liveUid);
                 if (empty($versionedUid)) {
                     if ($throwException) {
-                        throw new Exception('Versioned UID could not be determined', 1476049592);
+                        throw new Exception('Versioned UID of ' . $tableName . ':' . $liveUid . ' could not be determined', 1476049592);
                     }
                     continue;
                 }
